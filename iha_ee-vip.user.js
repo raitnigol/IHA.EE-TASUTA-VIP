4109d55764aa0ab2815debb4467b8a24e99926b3
Nigol <rait.nigol@khk.ee>
2020-06-04 15:26:33 +0300

    Update iha_ee-vip.user.js

--- iha_ee-vip.user.js
+++ iha_ee-vip.user.js
@@ -3,13 +3,33 @@
 // @version      0.1
 // @description  iha.ee eemalda gif (tasuta vip)
 // @author       Nigol
+// @run-at       document-end
 // @match        https://iha.ee/*
 // @include      https://*.iha.ee/*
+// @require http://code.jquery.com/jquery-3.4.1.min.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     window.onload = function(){
-        document.querySelector("body > div:nth-child(7) > table > tbody > tr:nth-child(2) > td > table:nth-child(2) > tbody > tr:nth-child(1) > td.center_td > table:nth-child(3) > tbody > tr:nth-child(2) > td > table:nth-child(3) > tbody > tr > td > table > tbody > tr:nth-child(2) > td > a > img").style.opacity = "0";
+        // eemalda gif pildilt
+        document.querySelector("body > div:nth-child(7) > table > tbody > tr:nth-child(2) > td > table:nth-child(2) > tbody > tr:nth-child(1) > td.center_td > table:nth-child(3) > tbody > tr:nth-child(2) > td > table:nth-child(3) > tbody > tr > td > table > tbody > tr:nth-child(2) > td > a > img").style.visibility = "hidden";
+
+        // leia pildi url
+        var imgURL = document.querySelector("body > div:nth-child(7) > table > tbody > tr:nth-child(2) > td > table:nth-child(2) > tbody > tr:nth-child(1) > td.center_td > table:nth-child(3) > tbody > tr:nth-child(2) > td > table:nth-child(3) > tbody > tr > td > table > tbody > tr:nth-child(2) > td");
+        imgURL = $(imgURL).css('background-image');
+        imgURL = imgURL.replace(/(url\(|\)|")/g, '');
+
+        // näita pildi urli pildi kohal ja ava uues aknas pilt
+        var showImgURL = document.getElementsByClassName("muu")[3];
+        var showImgURL_content = `<a href=${imgURL} target="_blank">${imgURL} | ava pilt uues aknas</a>`;
+        showImgURL.innerHTML = showImgURL_content;
+
+        // eemalda sisselogimise kirjad
+        var login_Top = document.querySelector("body > div:nth-child(7) > table > tbody > tr:nth-child(2) > td > table:nth-child(2) > tbody > tr:nth-child(1) > td.center_td > table:nth-child(3) > tbody > tr:nth-child(2) > td > table:nth-child(3) > tbody > tr > td > table > tbody > tr:nth-child(1)");
+        var login_Bottom = document.querySelector("body > div:nth-child(7) > table > tbody > tr:nth-child(2) > td > table:nth-child(2) > tbody > tr:nth-child(1) > td.center_td > table:nth-child(3) > tbody > tr:nth-child(2) > td > table:nth-child(3) > tbody > tr > td > table > tbody > tr:nth-child(3)");
+
+        login_Top.remove();
+        login_Bottom.remove();
     };
 })();
